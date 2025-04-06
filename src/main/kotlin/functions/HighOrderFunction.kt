5442bd60d70995cc6b94b9245fb1ca485ddf54a5
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-04-06 12:57:46 +0200

    Kotlin functions

--- src/main/kotlin/functions/HighOrderFunction.kt
+++ src/main/kotlin/functions/HighOrderFunction.kt
@@ -1,5 +1,50 @@
 package functions
 
 fun main() {
+    displayMessage(::morning)
+    displayMessage(::evening)
 
+    val action1 = selectAction(1)
+    println(action1(8, 5))// 13
+
+    val action2 = selectAction(2)
+    println(action2(8, 5))// 3
+}
+
+fun displayMessage(mes: () -> Unit) {
+    mes()
+}
+
+fun morning() {
+    println("Good Morning")
+}
+
+fun evening() {
+    println("Good Evening")
+}
+
+fun selectAction(key: Int): (Int, Int) -> Int {
+    // определение возвращаемого результата
+    when (key) {
+        1 -> return ::sumWhen
+        2 -> return ::subtract
+        3 -> return ::multiply
+        else -> return ::empty
+    }
+}
+
+fun empty(a: Int, b: Int): Int {
+    return 0
+}
+
+fun sumWhen(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun subtract(a: Int, b: Int): Int {
+    return a - b
+}
+
+fun multiply(a: Int, b: Int): Int {
+    return a * b
 }
